62372d2efef50169409d4330df680d22f6133a23
lubebul <rubys88684@gmail.com>
2018-05-15 15:29:59 +0800

    update description

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
-# [model = SEC.py | GAIN-SEC.py | GAIN-GCAM]
-python [model].py -g 0 -f 0.45 # training
-python3 [model].py -g 0 -f 0.05 -r 104999 -a inference # save predicted mask to disk
+# [model = GAIN-SEC.py | GAIN-GCAM.py]
+python [model].py -g 0 -f 0.99 -c
+python3 [model].py -g 0 -f 0.2 -r 104999 -a inference # save predicted mask to disk
 
 # tensorboard
-tensorboard --port 7778 --logdir=[model]-saver/sum
+tensorboard --port 7777 --logdir=[model]-saver/sum
