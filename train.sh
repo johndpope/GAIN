2948b64904c3e100b724eef0346353df0a47bf06
lubebul <rubys88684@gmail.com>
2018-05-30 17:16:01 +0800

    clean up

--- train.sh
+++
@@ -1,6 +0,0 @@
-# [model = GAIN-SEC.py | GAIN-GCAM.py]
-python [model].py -g 0 -f 0.99 -c
-python3 [model].py -g 0 -f 0.2 -r 104999 -a inference # save predicted mask to disk
-
-# tensorboard
-tensorboard --port 7777 --logdir=[model]-saver/sum
